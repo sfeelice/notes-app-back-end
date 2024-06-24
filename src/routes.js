cf8b566a0a3dedd279b40555990bcb454166d538
sleepyhydra12 <skolastika.fel2002@mail.ugm.ac.id>
2024-06-24 16:07:54 +0700

    fix bugs host value

--- src/routes.js
+++ src/routes.js
@@ -4,7 +4,7 @@ const routes=[
     {
         method: 'POST',
         path: '/notes',
-        handler: addNoteHandler,    // https://www.dicoding.com/academies/261/discussions/325473
+        handler: addNoteHandler,   
     },
     {
         method: 'GET',
